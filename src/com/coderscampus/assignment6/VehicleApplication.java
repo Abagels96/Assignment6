6ce51a5f6d1458bb0cd8fa5fa9a2c3d8ca3e90a9
Abagels96 <abmen18@gmail.com>
2024-11-07 21:03:06 -0600

    format

--- src/com/coderscampus/assignment6/VehicleApplication.java
+++ src/com/coderscampus/assignment6/VehicleApplication.java
@@ -3,10 +3,10 @@ package com.coderscampus.assignment6;
 import java.io.IOException;
 
 public class VehicleApplication {
-public static void main (String[]args) throws IOException {
-	VehicleService service= new VehicleService();
-	
-service.compileArray("src/modelX.csv","src/model3.csv","src/modelS.csv");
+	public static void main(String[] args) throws IOException {
+		VehicleService service = new VehicleService();
 
-}
+		service.compileArray("src/modelX.csv", "src/model3.csv", "src/modelS.csv");
+
+	}
 }
